baf15e0dedc773d5dd4b0be8c9619bff644e3344
Ton Tran 👻 <v-tranton@microsoft.com>
2021-12-28 23:45:48 -0800

    added firebase, storybook, and color tokens

---
+++ src/components/button/Button.stories.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Button} from './Button';
+
+export default {
+	title: 'Design System/Atoms/Button',
+	component: Button
+}
+
+export const Primary = () => (
+	<>
+		<Button>Primary</Button>
+		<Button outline>Primary</Button>
+	</>
+)
+
+export const Secondary = () => (
+	<>
+		<Button secondary>Secondary</Button>
+		<Button secondary outline>Secondary</Button>
+	</>
+)
+
+export const Success = () => (
+	<>
+		<Button success>Success</Button>
+		<Button success outline>Success</Button>
+	</>
+)
+
+export const Danger = () => (
+	<>
+		<Button danger>Danger</Button>
+		<Button danger outline>Danger</Button>
+	</>
+)
+
+
+export const Warning = () => (
+	<>
+		<Button warning>Warning</Button>
+		<Button warning outline>Warning</Button>
+	</>
+)
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,14 +2,32 @@ import React from 'react';
 import './Button.scss';
 
 interface IButton {
+  danger?: boolean;
   onClick?: (e: React.SyntheticEvent<HTMLButtonElement>) => void;
+  outline?: boolean;
+  secondary?: boolean;
+  success?: boolean;
+  warning?: boolean;
 }
 
-export const Button: React.FC<IButton> = ({children, onClick}) => {
+export const Button: React.FC<IButton> = ({
+  children,
+  danger,
+  onClick,
+  outline,
+  secondary,
+  success,
+  warning
+}) => {
 
   return (
     <button
-      className={`gin-button`}
+      className={`imagine-button ${
+        danger ? 'danger' :
+        secondary ? 'secondary' :
+        success ? 'success' :
+        warning ? 'warning' : 'primary'
+      } ${outline ? 'outline' : ''}`}
       onClick={onClick}
     >
       {children}
