45b41784d6aa6e81d07971903c0dab3ae58dac2b
Ton Tran 👻 <v-tranton@microsoft.com>
2021-12-31 10:31:25 -0800

    added ripple effect on button click

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -22,6 +22,33 @@ export const Button: React.FC<IButton> = ({
   warning
 }) => {
 
+  const createRipple = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const btn = e.currentTarget;
+    const circle = document.createElement('span');
+    const diameter = Math.max(btn.clientWidth, btn.clientHeight);
+    const radius = diameter / 2;
+
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${e.clientX - btn.offsetLeft - radius}px`;
+    circle.style.top = `${e.clientY - btn.offsetTop - radius}px`;
+    circle.classList.add('ripple');
+
+    const ripple = btn.querySelectorAll('.ripple')[0];
+    if(ripple){
+      ripple.remove()
+    };
+    btn.appendChild(circle);
+  }
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if(onClick){
+      onClick(e);
+    }
+    if(!e.currentTarget.className.includes('neumorphic')){
+      createRipple(e);
+    }
+  }
+
   return (
     <button
       className={`imagine-button ${
@@ -34,7 +61,7 @@ export const Button: React.FC<IButton> = ({
         outline ? 'outline' :
         neumorphic ? 'neumorphic' : ''
       }`}
-      onClick={onClick}
+      onClick={e => handleClick(e)}
     >
       {children}
     </button>
